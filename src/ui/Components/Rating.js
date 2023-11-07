8812ed105324b2eaeef1bdaa9d9332dbf67d9e29
prdb <prdb@spb-rtk.ru>
2023-11-07 10:08:04 +0300

    User can't drag rating stars

--- src/ui/Components/Rating.js
+++ src/ui/Components/Rating.js
@@ -1,7 +1,7 @@
 export const Rating = ({ rate }) => {
     const maskSize = rate / 5 * 100 + '%';
     return (
-        <div className={"w-100 d-flex justify-content-center position-relative user-select-none"} style={{height: "30px"}}>
+        <div className={"w-100 d-flex justify-content-center position-relative user-select-none pe-none"} style={{height: "30px"}}>
             <p className={"rating-stars"} style={{textShadow: "0 0 10px #00000066"}}>★★★★★</p>
             <p className={"rating-stars mask"} style={{color: "gold", maskSize: maskSize, WebkitMaskSize: maskSize}}>★★★★★</p>
         </div>
