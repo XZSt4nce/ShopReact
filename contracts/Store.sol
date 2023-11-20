978b3a0a1f9b721264741b10b9258675d7245571
prdb <prdb@spb-rtk.ru>
2023-11-20 18:11:00 +0300

    contract

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -1,34 +1,52 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-import "./IStore.sol";
+contract Store {
+    enum Role {
+        Admin,
+        User
+    }
+    struct User {
+        address wallet;
+        string name;
+        Role role;
+    }
 
-contract Store is IShop, IUsers {
+    struct Product {
+        string title;
+        uint256 price;
+        string description;
+        string category;
+        string image;
+        uint8 rateValue;
+        uint256 rateCount;
+        uint256 count;
+    }
+
     address private owner;
     Product[] private products;
 
-    mapping (address => CartProduct[]) private ownership;
-    mapping (string => CartProduct) private titleToCartProduct;
+    mapping (address => Product[]) private ownership;
     mapping (address => User) private addressToUser;
     mapping (string => address) private loginToAddress;
     mapping (string => string) private loginToPassword;
 
     modifier onlyRole(Role _role) {
-        require (addressToUser[msg.sender].role == _role, unicode"Недостаточно прав");
+        require (addressToUser[msg.sender].role == _role, "Access denied");
         _;
     }
 
     constructor(Product[] memory initProducts) {
         owner = msg.sender;
         _signUp("admin", "123", Role.Admin);
-        
+
         for (uint256 i = 0; i < initProducts.length; i++) {
             products.push(initProducts[i]);
         }
     }
 
     function signIn(string calldata login, string calldata password) external view returns(address wallet) {
-        require(_getHash(loginToPassword[login]) == _getHash(password));
+        require(_getHash(loginToPassword[login]) == _getHash(password), "Invalid data!");
         return (loginToAddress[login]);
     }
 
@@ -36,19 +54,19 @@ contract Store is IShop, IUsers {
         _signUp(login, password, Role.User);
     }
 
-    function buyProducts(CartProduct[] memory cartProducts) external payable {
+    function buyProducts(Product[] memory cartProducts) external payable onlyRole(Role.User) {
         uint256 orderPrice = 0;
         for (uint256 i = 0; i < cartProducts.length; i++) {
-            orderPrice += cartProducts[i].product.price * cartProducts[i].count;
+            orderPrice += cartProducts[i].price * cartProducts[i].count;
         }
-        require(msg.value >= orderPrice, unicode"Недостаточно средств");
+        require(msg.value >= orderPrice, "Insufficient funds!");
         payable(owner).transfer(msg.value);
 
-        CartProduct[] memory userProducts = ownership[msg.sender];
+        Product[] memory userProducts = ownership[msg.sender];
         for (uint256 i = 0; i < cartProducts.length; i++) {
             bool notFound = true;
             for (uint256 j = 0; j < userProducts.length; j++) {
-                if (_getHash(userProducts[j].product.title) == _getHash(cartProducts[i].product.title)) {
+                if (_getHash(userProducts[j].title) == _getHash(cartProducts[i].title)) {
                     notFound = false;
                     ownership[msg.sender][j].count += cartProducts[i].count;
                 }
@@ -77,13 +95,17 @@ contract Store is IShop, IUsers {
         return products;
     }
 
-    function getOwnProducts() external view returns(CartProduct[] memory) {
+    function getBalance() external view returns(uint256) {
+        return msg.sender.balance;
+    }
+
+    function getOwnProducts() external view returns(Product[] memory) {
         return ownership[msg.sender];
     }
 
     function _signUp(string memory login, string memory password, Role role) private {
-        require(addressToUser[msg.sender].wallet == address(0), unicode"Вы уже зарегистрированы");
-        require(addressToUser[loginToAddress[login]].wallet == address(0), unicode"Пользователь с таким именем уже зарегистрирован");
+        require(addressToUser[msg.sender].wallet == address(0), "You are already registered!");
+        require(addressToUser[loginToAddress[login]].wallet == address(0), "A user with this login is already registered!");
         addressToUser[msg.sender] = User(msg.sender, login, role);
         loginToAddress[login] = msg.sender;
         loginToPassword[login] = password;
